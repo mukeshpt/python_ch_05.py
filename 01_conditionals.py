30e75de5a6edb6b9766b05ea9e74c8bd0ede8c8b
Mukesh kumar <mukeshog14@gmail.com>
2025-07-03 00:27:43 +0530

    Add files via upload

---
+++ 01_conditionals.py
@@ -0,0 +1,12 @@
+# if, else statement
+a = int(input("Enter your age :"))
+if(a>=18) :
+    print(" You are above the age of concent")
+    print("good for you")
+
+
+
+else:
+    print(" You are below the age of concent")
+
+print(" end of the program")
---
+++ 02_if_elif_else_ladder.py
@@ -0,0 +1,19 @@
+# if,elif,else ladder
+a = int(input("Enter your age :"))
+if(a>=18) :
+    print(" You are above the age of concent")
+    print("good for you")
+
+elif(a<0):
+    print(" bacche abhi paida nhu hue ho ")
+
+elif(a==0):
+    print(" you are entring 0 which is not valid age")
+
+else:
+    print(" You are below the age of concent")
+
+print(" end of the program")
+
+
+                    
